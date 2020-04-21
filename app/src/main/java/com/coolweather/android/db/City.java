77315da3815dee4ed2d27d4a3685d4e69dfc3e27
caillsuper666 <caillsuper666@github.com>
2020-04-21 10:05:13 +0000

    Add Bing background

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -3,7 +3,6 @@ package com.coolweather.android.db;
 import org.litepal.crud.DataSupport;
 
 public class City extends DataSupport {
-
     private int id;
     private String cityName;
     private int cityCode;
--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -3,12 +3,10 @@ package com.coolweather.android.db;
 import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport {
-
     private int id;
     private String countyName;
     private String weatherId;
     private int cityId;
-
 
     public int getId() {
         return id;
